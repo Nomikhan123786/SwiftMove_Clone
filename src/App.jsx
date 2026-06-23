ad06632fc453738e6fd23f4bbe4b9d5754f86098
Nomikhan123786 <nomank7887682@gmail.com>
2026-06-23 13:06:51 +0500

    Add dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { useState } from "react";
-
+import { useEffect } from "react";
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  useEffect(() => {
+    const root = document.documentElement;
+    if (darkMode) {
+      root.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      root.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
   return (
     <>
       <BrowserRouter>
-        <Navbar />
+        <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
       </BrowserRouter>
     </>
   );
