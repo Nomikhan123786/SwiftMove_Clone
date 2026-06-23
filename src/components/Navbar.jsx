d8f1c7b8aabbc087bd252fba179c21133d0298f4
Nomikhan123786 <nomank7887682@gmail.com>
2026-06-23 15:38:19 +0500

    Full responsive and dark mode Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,26 @@
 import logo from "../assets/images/logo.svg";
+import light_logo from "../assets/images/logo-light.svg";
 import { Link } from "react-router-dom";
-import { Sun, Moon } from "lucide-react";
+import { Sun, Moon, Menu, CrossIcon } from "lucide-react";
+import { useState } from "react";
+
 const Navbar = ({ darkMode, setDarkMode }) => {
+  const [show, setShow] = useState(false);
+
   return (
     <header className="sticky top-0 z-50 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg transition-all dark:border-gray-800 dark:bg-gray-950/80">
-      <div className="container mx-auto flex py-1 ">
-        <div className="py-5 h-8 w-full lg:h-10 dark:hidden">
-          <img src={logo} alt="" />
+      <div className="container mx-auto flex h-16 items-center justify-between px-4 lg:h-20 lg:px-8 ">
+        <div className="flex items-center gap-2">
+          <img src={logo} alt="" className="h-8 w-auto lg:h-10 dark:hidden" />
+          <img
+            src={light_logo}
+            alt=""
+            className="hidden h-8 w-auto lg:h-10 dark:block"
+          />
         </div>
-        <div className="  hidden lg:flex lg:items-center lg:gap-8">
-          <ul className="flex [&>*]:flex-3 [&>*]:w-25 [&>*]:py-5 [&>*]:text-sm [&>*]:font-medium [&>*]:text-gray-600 [&>*]:transition-colors [&>*]:hover:text-gray-900 [&>*]:dark:text-gray-400 [&>*]:dark:hover:text-white ">
-            <li>
+        <div className="  hidden lg:flex lg:items-center lg:gap-3">
+          <ul className="flex gap-5">
+            <li className=" [&>*]:flex-3  [&>*]:py-5 [&>*]:text-sm [&>*]:font-medium [&>*]:text-gray-600 [&>*]:transition-colors [&>*]:hover:text-gray-900 [&>*]:dark:text-gray-400 [&>*]:dark:hover:text-white [&>*]:gap-100">
               <Link>About </Link>
             </li>
             <li>
@@ -39,16 +49,64 @@ const Navbar = ({ darkMode, setDarkMode }) => {
             </li>
           </ul>
         </div>
-        <div className="w-60  py-6 size-7">
-          <button onClick={() => setDarkMode(!darkMode)}>
-            {darkMode ? <Sun size={20} /> : <Moon size={20} />}
+        <div className="flex items-center  gap-5">
+          <div className="  py-1 size-7">
+            <button onClick={() => setDarkMode(!darkMode)}>
+              {darkMode ? <Sun size={20} /> : <Moon size={20} />}
+            </button>
+          </div>
+          <div className=" hidden sm:flex block w-40 h-13 rounded-lg bg-amber-600 px-5 my-3 text-center text-lg font-medium text-white transition-colors hover:bg-accent-600">
+            <button className="block w-35 rounded-lg bg-amber-600 px-5 py-0.5 text-center text-sm font-medium text-white transition-colors hover:bg-accent-600">
+              Free Qoute
+            </button>
+          </div>
+        </div>
+        <div className="lg-hidden">
+          <button onClick={() => setShow(!show)}>
+            {show ? (
+              <CrossIcon className="dark:text-white" />
+            ) : (
+              <Menu className="dark:text-white" />
+            )}
           </button>
         </div>
-        <div className=" hidden sm:flex block w-40 h-13 rounded-lg bg-amber-600 px-5 my-3 text-center text-lg font-medium text-white transition-colors hover:bg-accent-600">
-          <button className="block w-35 rounded-lg bg-amber-600 px-5 py-0.5 text-center text-sm font-medium text-white transition-colors hover:bg-accent-600">
-            Free Qoute
-          </button>
-        </div>
+      </div>
+      <div className="lg-hidden">
+        {show ? (
+          <div className="border-t border-gray-200 bg-white px-4 py-4 dark:border-gray-800 dark:bg-gray-950">
+            <ul className=" [&>*]:block [&>*]:py-3 [&>*]:text-base [&>*]:font-medium [&>*]:text-gray-900 [&>*]:dark:text-white">
+              <li>
+                <Link>About </Link>
+              </li>
+              <li>
+                <Link>Servies </Link>
+              </li>
+              <li>
+                <Link>How it Works</Link>
+              </li>
+              <li>
+                <Link>Areas </Link>
+              </li>
+              <li>
+                <Link>Pricing </Link>
+              </li>
+              <li>
+                <Link>Testimonials </Link>
+              </li>
+              <li>
+                <Link>Qoute </Link>
+              </li>
+              <li>
+                <Link>Blog </Link>
+              </li>
+              <li>
+                <Link>Contact </Link>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </header>
   );
